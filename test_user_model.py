be3312dcdc9c960bb6473180bfd3574a832f0f2c
edwinkim97 <edwinkim124@gmail.com>
2022-02-11 16:19:54 -0800

    ready to deploy, with test comments

--- test_user_model.py
+++ test_user_model.py
@@ -55,3 +55,45 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+    
+    # PLAN FOR ALL TESTS ####################    
+    # USER MODEL TESTS!!!! #
+    # def test_repr(self):
+    #    """Tests dunder repr for USER model"""
+
+    # Using u (newly created used), Then, test with 
+    # assertEqual(repr(testUser), "<User #{test.id}: {test.username}, {test.email}>") 
+    
+    
+    # def test_is_following(self):
+    # DOCSTRINGS
+    # Happy Case:
+    # Create user, second user, third user and create cases where user is following second user
+    # We can make user follow anotehr user by instatiatining Follows table
+    # follow = Follows(u1.id, u2.id) 
+    # Sad Case:
+    # follow = Follows(u1.id, u2.id)
+    # AssertIn u3 u1.followers
+    
+    # User signup, with test user, and then test with AssertIsInstance of USER class
+    # Negative class would test that the signup method returns a None, check with
+    # AssertIsInstance(returned_None, None)
+    
+    # For authentication, in happy case return user, check with AssertIsInstance
+    # for sad case, AssertEqual(returned_from_authenticate_method, False)
+    # checking for both invalid username and invalid password in 2 diff test cases
+    
+    # END OF USER MODEL TESTS #
+    
+    # TESTS FOR MESSAGE MODEL TESTS #####
+    
+    # Test to see instance of Message is actually an instance with
+    # AssertIsInstance (instance_of_Message, Message)
+    # Test with user_id not in Users.query.all() 
+    # Test with message that is over 140 characters in length
+    
+    # END OF MESSAGE MODEL TESTS ####
+    
+    # ROUTE TESTS FOR USER VIEW FUNCTION ###
+    
+    # 
