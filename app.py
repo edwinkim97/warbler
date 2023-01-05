3ea231a382c1ff229f0973fab6be92f1f457787a
edwinkim97 <edwinkim124@gmail.com>
2023-01-04 19:08:40 -0800

    replace postgres -> postgresql

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    "postgres://vcenrwhv:C7Ancg4Re1MC7QGag_NRiJ-bMyXhsDU_@heffalump.db.elephantsql.com/vcenrwhv")
+    "postgres://vcenrwhv:C7Ancg4Re1MC7QGag_NRiJ-bMyXhsDU_@heffalump.db.elephantsql.com/vcenrwhv").replace("postgres://", "postgresql://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
